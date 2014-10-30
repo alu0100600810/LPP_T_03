8572bddd414f694fa58cb6e97dde940b8c0b0f04
César Ravelo Martínez <alu0100511314@ull.edu.es>
2014-10-30 12:49:53 +0000

    Implementado el código para la extracción de la cabeza de la lista

--- lib/examen/examen.rb
+++ lib/examen/examen.rb
@@ -1,6 +1,21 @@
 require "pregunta"
 
 class Examen
+  attr_accessor :cabeza, :cola, :total
+
   Nodo = Struct.new :value, :next
 
+  def initialize(p)
+    @cabeza = Nodo.new(p, nil)
+    @cola = @cabeza
+    @total = 1
+  end
+
+  def pop
+    head = @cabeza
+    @cabeza = @cabeza.next
+    head.next = nil
+    @total -= 1
+    head.value
+  end
 end
