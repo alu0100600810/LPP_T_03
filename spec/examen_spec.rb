9d11a5e03a14c9e78f46fd65155a528e015f2a9b
Ayose Castillo Barroso <alu0100600810@ull.edu.es>
2014-10-30 11:24:36 +0000

    Implementar la prueba del nodo

--- spec/examen_spec.rb
+++ spec/examen_spec.rb
@@ -7,9 +7,10 @@ class Examen
 
     before :each do
       @q = Pregunta.new(:text => '2+2=', :right => 4 , :distractors => [9,3,5])
+      @n = Nodo.new(@q, nil)
     end
 
-    context "Cuando se construye una pregunta" do
+    context "Pregunta" do
       it "Debe tener texto y alguna pregunta" do
         expect(@q.text)=='2+2='
 	expect(@q.right)==4
@@ -17,10 +18,6 @@ class Examen
       it "debe tener 3 componentes" do
         expect {Pregunta.new(:text => '5*8=?')}.to raise_error(ArgumentError)	
       end
-	
-    end
-
-    context "Debe poder:" do
       it "mostrar pregunta" do
         expect(@q).to respond_to :text
       end
@@ -36,9 +33,17 @@ class Examen
       it "mostrarse correctamente" do
         expect(@q.to_s).to match(/(\d|\w)+\n(\d\)\s+(\w|\d)+\n)+/)
       end
-			
-    end		
 	
-  end
+    end
+    
+    context "Nodo" do
+      
+      it "Debe existir un nodo de la lista con sus datos y su siguiente" do
+         
+         expect(@n.value)==@p
+         expect(@n.next)==nil
 
+      end
+    end		
+  end			
 end
