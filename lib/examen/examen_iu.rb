6da507619b32a46a2a5df7f7764aae0e6dfd5812
César Ravelo Martínez <alu0100511314@ull.edu.es>
2014-11-20 15:01:28 +0000

    Implementado el código para evaluar un examen en la clase ExamenIu

--- lib/examen/examen_iu.rb
+++ lib/examen/examen_iu.rb
@@ -7,4 +7,21 @@ class ExamenIu
     @exam = Exam.new(pregunta)
   end
 
+  def test
+    result = Array.new(@exam.list.count, 0)
+    i = 0
+    acertadas = 0
+    @exam.list.each do |p|
+      pActual = p.to_s
+      puts (i + 1).to_s + ") " + pActual
+      STDOUT.flush
+      opc = pActual.match(%r{#{gets.chomp}\)\s+(.*)})[1]
+      result[i] = (p.right.to_s.eql?opc)?1:0
+      acertadas += 1 if (result[i] == 1)
+      puts "> Respuesta #{((result[i] == 1)?"correcta":"incorrecta") + " (" + acertadas.to_s + "/" + result.size.to_s + ")"}\n\n"
+      i += 1
+    end
+    result
+  end
+
 end
