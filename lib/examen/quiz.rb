8112d7662421edc8e6633b5fa2deb62ddf75291e
Ayose Castillo Barroso <alu0100600810@ull.edu.es>
2014-12-11 13:41:35 +0000

    Documentando la Clase Quiz

--- lib/examen/quiz.rb
+++ lib/examen/quiz.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 require "exam"
-
+#Clase gestora de un DSL
 class Quiz
   attr_accessor :exam, :count, :title
-
+  #Crea una Instancia de la clase Quiz, a partir de un título y un bloque 
   def initialize(title, &block)
     @title = title
     @exam = Exam.new(Pregunta.new(:text => "dummy", :right => "dummy", :distractors => ["dummy"]))
@@ -11,23 +11,23 @@ class Quiz
     @count = 0 
     instance_eval &block
   end 
-
+  #Devuelve el Simbolo :right
   def right
     :right
   end 
-  
+  #Devuelve un Array con el Simbolo :wrong y un indice
   def wrong
     @count += 1
     [:wrong, @count]
   end 
-
+  #Actualiza el examen actual, con una nueva pregunta
   def question(*args)
     distract = Array.new
     args[1].keys.each { |x| distract << args[1][x] if x.class.equal? Array }
     exam << Pregunta.new(:text => args[0], :right => args[1][:right], :distractors => distract)
     exam
   end 
-
+  #Definicion del Metodo to_s, para la Clase Quiz
   def to_s
     "\s\s#{@title}\n#{'#' * (@title.size + 4)}\n\n#{@exam}"
   end
